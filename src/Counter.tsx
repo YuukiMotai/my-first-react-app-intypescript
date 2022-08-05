c2f78ae6d5c6b723ea15fb24206026ec67bf0b89
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-08-05 12:52:24 +0900

    useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom'
 
  //type MySetStateAction = number | ((prevState: number) => number);
- type Dispatch = (value: number | ((prevState: number) => number)) => void;
+ //type Dispatch = (value: number | ((prevState: number) => number)) => void;
+ //const array: Array<number> = [1,2,3];
+ //const readonlyarray: ReadonlyArray<number> = [1,2,3];
+ //readonlyarray[0] = 11;
+
 
 const Counter: React.FC<{}> = () => {
   const [count, setCount] = useState<number>(0);
@@ -13,12 +17,21 @@ const Counter: React.FC<{}> = () => {
   const decrement = () => {
     setCount(count - 1);
   }
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1
+  });
 
   return (
-    <p>value: {count}
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-    </p>
+    <>
+      <p>value: {count}
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </p>
+      <p>
+        This component was re-renderd {renderTimes.current} times.
+      </p>
+    </>
   );
 };
 
